bfa6efab5a135703a007cd68f0f8f95d90db9d67
Artem Sorokin <102421671+ArtSN7@users.noreply.github.com>
2025-01-31 16:46:03 +0000

    profile header fixed

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import { FaUser, FaCog } from "react-icons/fa";
+import { FaCog } from "react-icons/fa";
 import "../styles/ProfileHeader.css"; 
 
 const ProfileHeader = ({username}) => {
   return (
     <div className="profile-header">
-      {/* Left Section */}
+
       <div className="profile-info">
-        <div className="profile-picture"></div>
+
+        <div className="profile-picture">
+          <img className="avatar" src="https://picsum.photos/200" />
+        </div>
 
         <div className="text-section">
           <p className="name">{username}</p>
         </div>
+
       </div>
 
-      {/* Icons */}
       <div className="icons">
-        <FaUser />
         <FaCog />
       </div>
+
     </div>
   );
 };
